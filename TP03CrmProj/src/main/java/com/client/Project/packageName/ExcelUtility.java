b4cb8bc6465d4f13f2cd8bd894180164f2bb81bc
Dell <Dell@DESKTOP-HHQ3HI3>
2026-05-18 12:23:22 +0530

    Excel utility created

---
+++ TP03CrmProj/src/main/java/com/client/Project/packageName/ExcelUtility.java
@@ -0,0 +1,18 @@
+package com.client.Project.packageName;
+
+import java.io.FileInputStream;
+
+import java.lang.String;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelUtility {
+	public  String getDataFromExcel(String Sheetname, int rownum, int cellnum) throws Throwable {
+		FileInputStream fis=new FileInputStream("");
+		Workbook wb = WorkbookFactory.create(fis);
+		 String data = wb.getSheet(Sheetname).getRow(rownum).getCell(cellnum).toString();
+		 return data;
+	}
+
+}
