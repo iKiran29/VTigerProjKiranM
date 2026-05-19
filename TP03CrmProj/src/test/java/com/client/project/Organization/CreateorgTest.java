0e3b5a26255e4bd9248274b7ad15e437c14cb7e5
Dell <Dell@DESKTOP-HHQ3HI3>
2026-05-19 10:52:54 +0530

    Merge branch 'master' into LocalBranch

--- TP03CrmProj/src/test/java/com/client/project/Organization/CreateorgTest.java
+++ TP03CrmProj/src/test/java/com/client/project/Organization/CreateorgTest.java
@@ -1,10 +1,12 @@
 package com.client.project.Organization;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.client.ObjectRepository.HomePage;
 import com.client.ObjectRepository.Orgpage;
 import com.client.Project.packageName.BaseClass;
+
 
 
 /**
@@ -15,17 +17,34 @@ import com.client.Project.packageName.BaseClass;
 
 public class CreateorgTest extends BaseClass {
 	@Test
-	public void createOrgTest() {
+	public void createOrgTest() throws Throwable {
+		
+		String orgName =eLib.getDataFromExcel("org", 1, 2)+jLib.getRandomNum();
 				
 		HomePage hp=new HomePage(driver);
 		hp.getOrgLink().click();
 		
 	
-		Orgpage op1=new Orgpage(driver);
-	  	op1.getCreateNewOrgBtn().click();
+		Orgpage op=new Orgpage(driver);
+	  	op.getCreateNewOrgBtn().click();
+	  	op.createOrg(orgName);
+	    //op.getStreetEdt().sendKeys("Mumbai");
+	    
+  		//String actOrgName = op.getHeadermsg().getText();
+//  		if(actOrgName.equals(orgName)) {
+//  			System.out.println(orgName+"name is verified==PASS");
+//  		}else {
+//  			System.out.println(orgName+"name is not verified==FAIL");
+//  		      }
+  		
+  		String FB  = op.getHeadermsg().getText();
+		System.out.println(FB);
+		Assert.assertEquals("FB","fb1");
+  		hp.logout();
+  		
+  		
+   }
 	  	
-	  	
-		
-	}
-		
 }
+		
+
