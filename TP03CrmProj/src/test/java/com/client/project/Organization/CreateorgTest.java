e0f7426d34c67ed7d98ea6344fe9c3d82fe7a8b2
Dell <Dell@DESKTOP-HHQ3HI3>
2026-05-19 11:16:22 +0530

    updated base class and organization module

--- TP03CrmProj/src/test/java/com/client/project/Organization/CreateorgTest.java
+++ TP03CrmProj/src/test/java/com/client/project/Organization/CreateorgTest.java
@@ -19,36 +19,30 @@ public class CreateorgTest extends BaseClass {
 	@Test
 	public void createOrgTest() throws Throwable {
 		
+		/*read test script data from excel file*/
 		String orgName =eLib.getDataFromExcel("org", 1, 2)+jLib.getRandomNum();
-				
+		
+		/*step2:navigate to organization homepage module*/
 		HomePage hp=new HomePage(driver);
 		hp.getOrgLink().click();
 		
-	
+
+	  	/*step-3: click on "create organization" button*/
 		Orgpage op=new Orgpage(driver);
 	  	op.getCreateNewOrgBtn().click();
+	  	
+	  	 /*step-4:-enter all the details and create new organization using buisness method*/
 	  	op.createOrg(orgName);
-	    //op.getStreetEdt().sendKeys("Mumbai");
 	    
-  		//String actOrgName = op.getHeadermsg().getText();
-//  		if(actOrgName.equals(orgName)) {
-//  			System.out.println(orgName+"name is verified==PASS");
-//  		}else {
-//  			System.out.println(orgName+"name is not verified==FAIL");
-//  		      }
-  		
-  		String FB  = op.getHeadermsg().getText();
-		System.out.println(FB);
-		Assert.assertEquals("FB","fb1");
-  		hp.logout();
-  		
+	  	/*verify the header as expected result*/
+ 		String HeaderMsg = op.getHeadermsg().getText();
+ 		boolean status = HeaderMsg.contains(orgName);
+		System.out.println(HeaderMsg);
+		Assert.assertEquals(status, true);
   		
    }
-
-
-		
 	
-	}
+}
 
 		
 
