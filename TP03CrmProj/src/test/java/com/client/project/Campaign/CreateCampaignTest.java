a033995f10af8184973c5e00e395d19be2a85a51
nnakhade13 <nnakhade13@gmail.com>
2026-05-22 11:20:46 +0530

    updated campaign page

--- TP03CrmProj/src/test/java/com/client/project/Campaign/CreateCampaignTest.java
+++ TP03CrmProj/src/test/java/com/client/project/Campaign/CreateCampaignTest.java
@@ -1,9 +1,11 @@
 package com.client.project.Campaign;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.client.ObjectRepository.CampaignPage;
+import com.client.ObjectRepository.Orgpage;
 import com.client.Project.packageName.BaseClass;
 
 public class CreateCampaignTest extends BaseClass {
@@ -24,6 +26,27 @@ public class CreateCampaignTest extends BaseClass {
 		Assert.assertEquals(status, true);
 		
 	}
+	
+    @Test
+    
+    public void deleteCampaignTest() {
+    	hp.getMorelink().click();
+		hp.getCampaignlink().click();
+		
+    	driver.findElement(By.id("537")).click();
+    	Orgpage op=new Orgpage(driver);
+		op.getEditorg().click();
+		
+		CampaignPage cmp = new CampaignPage(driver);
+		String actualMsg = cmp.getEditheaderInfocampaign().getText();
+
+	Assert.assertTrue(actualMsg.contains("Editing Campaign Information"));
+
+	System.out.println("Edit message verified successfully");
+	
+    	
+    }
+ 
 	
 
 }
